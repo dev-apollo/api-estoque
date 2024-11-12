f423edeff54ae5ae316678357df699e95f19e664
Apollo Santos <nobitagain@gmail.com>
2024-11-12 13:29:27 -0300

    2° Update
    - Operações do banco de dados em memória completas
    - Operações da API completas
    - Yup inserido para a validação dos dados nos processos de POST e PUT

--- database.js
+++ database.js
@@ -8,6 +8,42 @@ export class dbmemory {
     }
 
     select_all(){
-        return Array.from(this.#produtos.values())
+        return Array.from(this.#produtos.entries()).map((produtos) => {
+            const id = produtos[0]
+            const valores = produtos[1]
+            return {
+                id,
+                ...valores
+            }
+        })
+    }
+
+    select_one_by_id(id_inserido){
+        const produto = this.#produtos.get(id_inserido)
+        if(produto){
+            return {
+                id: id_inserido,
+                ...produto
+            }
+        }
+        return null
+    }
+
+    delete(id_inserido){
+        if(this.#produtos.has(id_inserido)){
+            this.#produtos.delete(id_inserido)
+            return true
+        }else{
+            return false
+        }
+    }
+
+    update(id_inserido, produto){
+        if(this.#produtos.has(id_inserido)){
+            this.#produtos.set(id_inserido, produto)
+            return true
+        }else{
+            return false
+        }
     }
 }
--- server.js
+++ server.js
@@ -1,35 +1,77 @@
 import { fastify } from "fastify"
+import * as yup from "yup"
 import { dbmemory } from "./database.js"
 
 const server = fastify()
 const db = new dbmemory()
 const PORT = 3333
 
-server.post("/produtos", (req, reply) => {
+let schema = yup.object().shape({
+    nome: yup.string().required(),
+    preco: yup.number().required().positive(),
+    quantidade: yup.number().required().integer().moreThan(-1)
+})
+
+server.post("/produtos", async (req, reply) => {
     const { nome, preco, quantidade } = req.body
-    db.insert({
-        nome,
-        preco,
-        quantidade
-    })
-    return reply.status(201).send()
+    try {
+        await schema.validate({ nome, preco, quantidade })
+        db.insert({
+            nome,
+            preco,
+            quantidade
+        })
+        return reply.status(201).send({message: "Produto cadastrado."})
+    }catch(error){
+        return reply.status(400).send({ message: "Informações fora do padrão aceito." })
+    }
 })
 
 server.get("/produtos", (req, reply) => {
     const produtos = db.select_all()
-    return produtos
+    if(produtos.length > 0){
+        return produtos
+    }else{
+        reply.status(200).send({message: "Não há produtos cadastrados."})
+    }
 })
 
 server.get("/produtos/:id", (req, reply) => {
-
-})
-
-server.put("/produtos/:id", (req, reply) =>{
-
+    const produto = db.select_one_by_id(req.query.id)
+    if(produto){
+        return produto
+    }else{
+        reply.status(404).send({message: "Produto não encontrado."})
+    }
 })
 
 server.delete("/produtos/:id", (req, reply) => {
+    const produto = db.delete(req.query.id)
+    if(produto){
+        reply.status(200).send({message: "Produto deletado."})
+    }else{
+        reply.status(404).send({message: "Produto não encontrado."})
+    }
+})
 
+server.put("/produtos/:id", async (req, reply) =>{
+    const id = req.query.id
+    const { nome, preco, quantidade } = req.body
+    try{
+        await schema.validate({ nome, preco, quantidade })        
+        const produto = db.update(id, {
+            nome,
+            preco,
+            quantidade
+        })
+        if(produto){
+            return reply.status(200).send({message: "Produto atualizado."})
+        }else{
+            reply.status(404).send({message: "Produto não encontrado."})
+        }
+    }catch(error){
+        reply.status(400).send({message: "Informações fora do padrão aceito."})
+    }
 })
 
 server.listen({
